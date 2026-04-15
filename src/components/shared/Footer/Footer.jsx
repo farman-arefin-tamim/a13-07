2b20b4a3c39f0bb01487c8b7bae03d80d7739209
farman-arefin-outlook <farman.arefin1@outlook.com>
2026-04-15 15:59:52 +0600

    Completed footer

--- src/components/shared/Footer/Footer.jsx
+++ src/components/shared/Footer/Footer.jsx
@@ -7,24 +7,29 @@ const Footer = () => {
   return (
     <footer className="bg-[#244d3f] pt-16 pb-10">
       <div className="max-w-7xl mx-auto px-6">
-        <div className="grid grid-cols-1 md:grid-cols-12 gap-12">
+        <div className="my-8">
           {/* Logo & Description */}
-          <div className="md:col-span-5">
-            <div className="flex items-center gap-3 mb-6">
-             
-            </div>
-             <h2 className="text-6xl font-bold tracking-tighter text-white text-center">
+          <div className="py-4">
+        
+             <h2 className="text-6xl font-bold tracking-tighter text-white text-center pb-4">
                 KeenKeeper
               </h2>
-            <p className="text-zinc-400 text-lg max-w-md">
-              Premium digital tools for creators, professionals, and businesses. Work smarter with our suite of powerful tools.
+            <p className="text-white text-center">
+                Your personal shelf of meaningful connections. Browse, tend, and nurture the relationships that matter most.
             </p>
           </div>
         </div>
+        {/* social links */}
+        <h2 className="font-semibold text-center text-white text-xl pb-2">Social Links</h2>
+        <div className="flex justify-center items-center gap-4">
+            <img src={instagram} alt="" />
+            <img src={facebook} alt="" />
+            <img src={twitter} alt="" />
+        </div>
 
         {/* Bottom Bar */}
-        <div className="mt-16 pt-8 border-t border-zinc-800 flex flex-col md:flex-row items-center justify-between gap-4 text-sm text-zinc-500">
-          <div>© {new Date().getFullYear()} DigiTools. All rights reserved.</div>
+        <div className="mt-16 pt-8 border-t border-gray-500 flex flex-col md:flex-row items-center justify-between gap-4 text-sm text-[#fafafa]">
+          <div>© {new Date().getFullYear()} KeenKeeper. All rights reserved.</div>
 
           <div className="flex gap-6">
             <a href="#" className="hover:text-red-400 transition">
